dcddc34c43a106d5635243c24cc76e0e0b79e4fd
Ricardo Antonio Centeno Cardenas <rcenteno@c2r3s6.42madrid.com>
2024-12-05 14:30:41 +0100

    approuching printf, first day, gotta go fast

---
+++ libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcenteno <rcenteno@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/13 14:56:39 by rcenteno          #+#    #+#             */
+/*   Updated: 2024/12/05 14:06:41 by rcenteno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
