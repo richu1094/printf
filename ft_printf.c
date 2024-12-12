23b807d44469824fb615d1d2730d177ec2e230fb
Ricardo Antonio Centeno Cardenas <rcenteno@c2r3s6.42madrid.com>
2024-12-12 17:14:22 +0100

    progress

--- ft_printf.c
+++ ft_printf.c
@@ -6,21 +6,106 @@
 /*   By: rcenteno <rcenteno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 13:57:44 by rcenteno          #+#    #+#             */
-/*   Updated: 2024/12/05 16:22:12 by rcenteno         ###   ########.fr       */
+/*   Updated: 2024/12/12 17:08:44 by rcenteno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+#include "ftprintf.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	index;
+
+	index = 0;
+	while (str[index] != '\0')
+		index++;
+	return (index);
+}
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	index;
+	size_t	length;
+
+	index = 0;
+	length = ft_strlen(s);
+	while (index < length)
+	{
+		write(fd, &s[index], 1);
+		index++;
+	}
+}
+
+void	ft_bzero(void *s, size_t len)
+{
+	unsigned char	*ptr;
+	size_t			index;
+
+	ptr = (unsigned char *)s;
+	index = 0;
+	while (index < len)
+		ptr[index++] = 0;
+}
+
+void	handle_char(char c)
+{
+	ft_putchar_fd(c, 1);
+}
+
+void	handle_string(char *str)
+{
+	ft_putstr_fd(str, 1);
+}
+
+void	handle_pointer(void *p)
+{
+	unsigned long	ptr;
+	char			hex_digits[] = "0123456789abcdef";
+	char			hex[16];
+	int				i;
+
+	write(1, "0x", 2);
+	ptr = (unsigned long)p;
+	if (ptr == 0)
+	{
+		write(1, "0", 1);
+		return ;
+	}
+	i = 15;
+	while (ptr > 0)
+	{
+		hex[i--] = hex_digits[ptr % 16];
+		ptr /= 16;
+	}
+	write(1, hex + i + 1, 16 - i - 1);
+}
+
+// void	handle_int(int n)
+// {
+// }
+
+// void	handle_unsigned_int(unsigned int n)
+// {
+// }
+
+// void	handle_hex_lower(unsigned int n)
+// {
+// }
+
+// void	handle_hex_upper(unsigned int n)
+// {
+// }
 
 void	test(char *str, ...)
 {
 	va_list	args;
-	int		n;
 
-	// char	*start;
-	// int		index;
 	va_start(args, str);
-	// start = str;
 	while (*str)
 	{
 		if (*str == '%')
@@ -28,48 +113,36 @@ void	test(char *str, ...)
 			str++;
 			if (*str == 'c')
 			{
-				printf("soy caracter\n");
+				handle_char(va_arg(args, int));
 			}
 			else if (*str == 's')
 			{
-				printf("soy string\n");
+				handle_string(va_arg(args, char *));
 			}
 			else if (*str == 'p')
 			{
-				printf("soy hexa\n");
+				handle_pointer(va_arg(args, void *));
 			}
-			else if (*str == 'd')
-			{
-				n = va_arg(args, int);
-				printf("numero: %d\n", n);
-			}
-			else if (*str == 'i')
-			{
-				printf("soy entero base 10\n");
-			}
-			else if (*str == 'u')
-			{
-				printf("soy decimal sin signo\n");
-			}
-			else if (*str == 'x')
-			{
-				printf("soy hexadecimal en min\n");
-			}
-			else if (*str == 'X')
-			{
-				printf("soy hexadecimal en mayus\n");
-			}
-			else if (*str == '%')
-			{
-				printf("soy porcentaje\n");
-			}
-			else
-			{
-				printf("done\n");
-			}
-			// index = str - start;
-			// printf("Posicion: %d", index);
-			// break ;
+			// else if (*str == 'd' || *str == 'i')
+			// {
+			// 	handle_int(va_arg(args, int));
+			// }
+			// else if (*str == 'u')
+			// {
+			// 	handle_unsigned_int(va_arg(args, unsigned int));
+			// }
+			// else if (*str == 'x')
+			// {
+			// 	handle_hex_lower(va_arg(args, unsigned int));
+			// }
+			// else if (*str == 'X')
+			// {
+			// 	handle_hex_upper(va_arg(args, unsigned int));
+			// }
+			// else if (*str == '%')
+			// {
+			// 	handle_percent();
+			// }
 		}
 		else
 		{
@@ -77,10 +150,11 @@ void	test(char *str, ...)
 		}
 		str++;
 	}
+	va_end(args);
 }
-
 int	main(void)
 {
-	char str[] = "hola %d chao";
-	test(str, 1234);
+	char str[] = "string: %s char: %c puntero: %p";
+	test(str, "hola mundo", "a", str);
+	printf("\n%p", str);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -1,21 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rcenteno <rcenteno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 14:56:39 by rcenteno          #+#    #+#             */
-/*   Updated: 2024/12/05 14:06:41 by rcenteno         ###   ########.fr       */
+/*   Updated: 2024/12/12 17:12:23 by rcenteno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFTPRINTF_H
-# define LIBFTPRINTF_H
+#ifndef FTPRINTF_H
+# define FTPRINTF_H
 
 # include <stdarg.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+
+// int	ft_printf(char const *, ...);
 
 #endif
